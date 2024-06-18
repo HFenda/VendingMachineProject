d07fcef75ede89863f36ad5d481220650a5c06f7
HFenda <harun.aliefendic.ha@gmail.com>
2024-06-18 16:10:09 +0200

    Izmjene na osnovu komentara

--- routers/items.py
+++ routers/items.py
@@ -1,6 +1,6 @@
-from fastapi import Body, APIRouter, HTTPException
-from pydantic import BaseModel,Field
-from typing import Optional
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel, Field
+from typing import Optional, List
 
 router = APIRouter(
     prefix="/items",
@@ -8,73 +8,60 @@ router = APIRouter(
     responses={401: {"item": "Not found"}}
 )
 
-
-class Item:
-    id: int
-    title: str
-    brand: str
-    price: float
-    quantity: int
-
-    def __init__(self, id: int, title: str, brand: str, price: float, quantity: int):
-        self.id = id
-        self.title = title
-        self.brand = brand
-        self.price = price
-        self.quantity = quantity
-
-class ItemRequest(BaseModel):
-    id: Optional[int]=None
-    title: str =Field(min_length=3, max_length=30)
+class Item(BaseModel):
+    id: Optional[int] = None
+    title: str = Field(min_length=3, max_length=30)
     brand: str = Field(min_length=3, max_length=30)
     price: float = Field(gt=0)
     quantity: int = Field(gt=-1, lt=21)
 
-ITEMS=[
-    Item(1,"Chocolate bar", "Dorina" , 1.5,  15),
-    Item( 2,"Potato chips",  "Chio",  2.4,  8),
-    Item( 3, "Soda",  "Coca-Cola",  1.2,  10),
-    Item( 4, "Water", "Oaza",  0.8,  9),
-    Item( 5, "Candy",  "Skittles",  1.5,  8),
-    Item( 6, "Crackers",  "Tuc",  1.8,  6),
-    Item( 7, "Energy drink", "Red Bull",  2.2,  8),
-    Item( 8, "Ice Tea",  "Lipton", 1.0,  6)
+ITEMS: List[Item] = [
+    Item(id=1, title="Chocolate bar", brand="Dorina", price=1.5, quantity=15),
+    Item(id=2, title="Potato chips", brand="Chio", price=2.4, quantity=8),
+    Item(id=3, title="Soda", brand="Coca-Cola", price=1.2, quantity=10),
+    Item(id=4, title="Water", brand="Oaza", price=0.8, quantity=9),
+    Item(id=5, title="Candy", brand="Skittles", price=1.5, quantity=8),
+    Item(id=6, title="Crackers", brand="Tuc", price=1.8, quantity=6),
+    Item(id=7, title="Energy drink", brand="Red Bull", price=2.2, quantity=8),
+    Item(id=8, title="Ice Tea", brand="Lipton", price=1.0, quantity=6)
 ]
 
-def find_item_id(item: Item):
-    if len(ITEMS)>0:
-        item.id = ITEMS[-1].id+1
+def find_item_id():
+    if ITEMS:
+        id = ITEMS[-1].id + 1
     else:
-        item.id=1
-
-    return item
+        id = 1
+    return id
 
 @router.get("/all-items")
 async def get_all_items():
     return ITEMS
 
 @router.get("/{item_brand}")
-async def get_all_items(item_brand: str):
+async def get_item_by_brand(item_brand: str):
     for item in ITEMS:
         if item.brand.casefold() == item_brand.casefold():
             return item
     raise HTTPException(status_code=404, detail="Item not found")
 
 @router.post("/add-item")
-async def create_item(item_request: ItemRequest):
-    new_item=Item(**item_request.dict())
-    ITEMS.append(find_item_id(new_item))
+async def create_item(item_request: Item):
+    new_item = item_request
+    new_item.id = find_item_id()
+    ITEMS.append(new_item)
+    return new_item
 
 @router.put("/update-item")
-async def update_item(item_request: ItemRequest):
-    updated_item=Item(**item_request.dict())
+async def update_item(item_request: Item):
     for i in range(len(ITEMS)):
-        if ITEMS[i].brand.casefold() == updated_item.brand.casefold():
-            ITEMS[i]= updated_item
+        if ITEMS[i].id == item_request.id:
+            ITEMS[i] = item_request
+            return item_request
+    raise HTTPException(status_code=404, detail="Item not found")
 
 @router.delete("/delete-item/{item_brand}")
 async def delete_item(item_brand: str):
     for i in range(len(ITEMS)):
         if ITEMS[i].brand.casefold() == item_brand.casefold():
-            ITEMS.pop(i)
-            break
+            return ITEMS.pop(i)
+    raise HTTPException(status_code=404, detail="Item not found")
--- routers/users.py
+++ routers/users.py
@@ -1,4 +1,4 @@
-from fastapi import Body, APIRouter,HTTPException
+from fastapi import Body, APIRouter, HTTPException
 from routers import items
 from pydantic import BaseModel
 
@@ -8,22 +8,15 @@ router = APIRouter(
     responses={401: {"user": "Can't find user"}},
 )
 
-class User:
+class User(BaseModel):
     type: str
     username: str
     password: str
 
-    def __init__(self, type: str, username: str, password: str):
-        self.type = type
-        self.username = username
-        self.password = password
-
-USERS=[
-    User( "admin",  "admin1",  "admin1"),
-    User( "user", "user1",  "user1")
+USERS = [
+    User(type="admin", username="admin1", password="admin1"),
+    User(type="user", username="user1", password="user1")
 ]
-
-
 
 class ItemRequest(BaseModel):
     name: str
@@ -31,30 +24,35 @@ class ItemRequest(BaseModel):
 
 @router.post("/purchase/{user_type}")
 async def purchase(user_type: str, purchase_item: ItemRequest):
-    if user_type=="user":
+    if user_type == "user":
         for item in items.ITEMS:
-            if item.brand.casefold()==purchase_item.name.casefold():
+            if item.brand.casefold() == purchase_item.name.casefold():
                 if item.quantity >= purchase_item.quantity:
-                    item.quantity -=purchase_item.quantity
+                    item.quantity -= purchase_item.quantity
                     return {"message": "Item has been purchased"}
                 raise HTTPException(status_code=400, detail="Not enough stock available")
+        raise HTTPException(status_code=404, detail="Item not found")
     raise HTTPException(status_code=400, detail="That user can't purchase items")
 
 @router.post("/restock/{user_type}")
-async def purchase(user_type: str, restock_item: ItemRequest):
-    if user_type=="admin":
+async def restock(user_type: str, restock_item: ItemRequest):
+    if user_type == "admin":
         for item in items.ITEMS:
-            if item.brand.casefold()==restock_item.name.casefold():
+            if item.brand.casefold() == restock_item.name.casefold():
                 if item.quantity < 20:
-                    item.quantity +=restock_item.quantity
+                    item.quantity += restock_item.quantity
                     return {"message": "Item has been successfully restocked"}
-                raise HTTPException(status_code=400, detail="Not enough stock available")
+                raise HTTPException(status_code=400, detail="Stock exceeds maximum limit")
+        raise HTTPException(status_code=404, detail="Item not found")
     raise HTTPException(status_code=400, detail="That user can't restock items")
 
 @router.get("/total-revenue/{user_type}/{brand}")
 async def total_revenue(user_type: str, brand: str):
-    if user_type=="admin":
+    if user_type == "admin":
         for item in items.ITEMS:
-            if item.brand.casefold()==brand.casefold():
-                return (20-item.quantity)*item.price
+            if item.brand.casefold() == brand.casefold():
+                total_sold = 20 - item.quantity
+                revenue = total_sold * item.price
+                return {"total_revenue": revenue}
+        raise HTTPException(status_code=404, detail="Item not found")
     raise HTTPException(status_code=400, detail="You don't have access to those information")
